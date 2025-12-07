e3d05d1a7cfa147fb117d00e6e40b0e3add140e3
Mohit Sonale <mohitsonale@gmail.com>
2025-12-08 01:00:56 +0530

    index

--- mern-auth/client/src/App.jsx
+++ mern-auth/client/src/App.jsx
@@ -10,10 +10,10 @@ function App(){
   return(
     <div>
 
-      {/* <Link to='/'>Home</Link>
-      <Link to='/'>Login</Link>
       <Link to='/'>Home</Link>
-      <Link to='/'>Home</Link> */}
+      <Link to='/login'>Login</Link>
+      <Link to='/email-verify'>Email-Verify</Link>
+      <Link to='/reset-password'>Reset-Password</Link>
 
       <Routes>
         <Route path='/' element={<Home />} />
